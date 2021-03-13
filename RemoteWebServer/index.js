d3c71f5a41349594973c64d5d3beb402d69ee54b
Eyup3 <eyup.ozturk@outlook.de>
2021-03-13 08:19:21 +0100

    Finally it works

--- RemoteWebServer/index.js
+++ RemoteWebServer/index.js
@@ -11,23 +11,23 @@ app.get("/", (req, res) => {
 const PATH = process.argv[2];
 
 //#region app.get paths
-app.post("/add1x", (req, res) => {
+app.get("/add1x", (req, res) => {
     IPC.write("add1x", PATH);
     res.redirect("/");
 });
-app.post("/sub1x", (req, res) => {
+app.get("/sub1x", (req, res) => {
     IPC.write("sub1x", PATH);
     res.redirect("/");
 });
-app.post("/add1y", (req, res) => {
+app.get("/add1y", (req, res) => {
     IPC.write("add1y", PATH);
     res.redirect("/");
 });
-app.post("/sub1y", (req, res) => {
+app.get("/sub1y", (req, res) => {
     IPC.write("sub1y", PATH);
     res.redirect("/");
 });
-app.post("/click", (req, res) => {
+app.get("/click", (req, res) => {
     IPC.write("click", PATH);
     res.redirect("/");
 });
