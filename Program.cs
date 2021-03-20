778080f19ed30ec0323ed2d2f7637165897455c2
Eyup3 <eyup.ozturk@outlook.de>
2021-03-20 11:15:57 +0100

    Fin(), Expose to private network missing

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿public class Program
+﻿using System.Threading;
+using System.Net;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        new WebServer().Start("http://192.168.2.169", 4000);
+        
+        new WebServer().Start("192.168.2.169", 4000);
         new TcpLiveServer().Start("192.168.2.169", 4001);
     }
 }
